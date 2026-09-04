d85b94c20c37d99d64da1d4a529f0ca3a07a1987
Márcio Leandro de Almeida Malaquias <leomalaj10@gmail.com>
2026-09-04 01:01:44 -0300

    data: add September 2026 roster payload part 4

---
+++ app/src/main/java/com/example/data/roster/RosterPayloadPart4.kt
@@ -0,0 +1,34 @@
+package com.example.data.roster
+
+internal object RosterPayloadPart4 {
+    const val VALUE =
+        "SdbE8r+NyuGt9Tg6FxuJDLPrNfHbp4PUL0OYTpR6hKkzslNGSBZa7BUvJ9IEr0Sl3Lwlm5nQDyq6r4PQvlBXbxYP5RHtgEpyr5JJbDnb386Ba30yEcVcKCQL" +
+        "7MK6toBExx6w7SDP7wyJYwgaCuxuskXOztSsZOA1rG7Chv203WEM9Nxj25QPGJpCpr/EImaNEQCFV9NQxZijd7+BFc0BkZ9TROSP7EbsT8WA6DviUCq3584/" +
+        "UDBaCxkxO0+74BJff+DRGimaBC87p/FL5WZ+Yce9RzJud14xvaBY+Pyccf3ISQ9Alzbz5cAIALcLUOWEWYC9Zw8AQPHzlwluE6Uqx3Wc1YYnDatphZOIeYwU" +
+        "4hHutT9bPXQE/4UYp1Y1q9EY6wNYsRgiM4y4XtM+Rvv4aMLTlFlvN1qdu0trOQi8Gn4mMrFdhJfyM83ftv0GlKEyr+hBtHz0Etdzf6b5lY4EjxN2LSI553EN" +
+        "dPa7LDxpt/8Tilav3yOvQOIoRB1yHHRWtL3ZDCxear6haZpCmolGQHcsHp7bOV8qivalEztynm6TxgPc8z7Ya4VM2Hse8XguM/P3JIBG1JkdGAGwuxVvhZ5V" +
+        "+cxCPiKIxLuGyNzbT8Emmb5bVbj0TClDO6n2/wdE3okw4Oiea75QtF43Yk7co2vdLEGfs8tC6IWwR1gBySOZMB+wsMwtS1KN2c/qmna47cp4A4TnAOZP0qqb" +
+        "k1UplaNVN/0WUNGP4bBu43bOzFWNebxUG/FIGa3XSOodHOjfLjD7W/5Q9keYkkZ3BSTGG8ckb2wf5IVcLJCxXRir2dwdwXXds6OyBJ9pFTfpx+y0pkQY6iyd" +
+        "rxDhV+9lJyFMrHFHqf8AcpM5at0v43w7nEkKrQWtttnWJAZnivk64iu1sT5vqbXozOLtRB2qW4YdPnMmo6VQWfL6a5JOeoj2rM48UvDFdWjDlUgL36alFrT2" +
+        "0BAudHqQ7JoyDKcWTb1nuAbvRI884H1rbGmjTU7ytSb3AoQodeSggmyZiTh9ZCNFdOKnwrQenUIbFrrrD9ZOeV/Zcl3f9TVsVnfetgNW+6jUHuPZ3Gg0t+KW" +
+        "6oMJlfgCoq0k3X3W4OaKZgAIlPSbdEWioOQqSzY8FmwkRbx8dlpcoxvRVT7HWs5XB6NEC+MzCdOAvveRMM/GOhGf2LVaZ4m4L5nK0OrPRHyE4uYLGVvShKfm" +
+        "mUOzun6DWmeNYRVWHVLYhmPtAS88pekqkoVEY2qSOxLF3L65mtmG3OZSEMWNOFUgk9zoQpmrVCw5u3oQq38mQAv2pporEcvPbJrV4c3GwpoIj7Ky9Gxa3WYz" +
+        "6IK+syHo6VxujHSTjcLIWCmsXAOXEgdes1neof8DNIjuz6K1tpHHOSIpDUCvDriM5/JlPm4lmmQf2WWmYRtiV/h2RHtr3HZE3H5fFgY4Wi6WgmgX0qV5loQ4" +
+        "COrtJuHm2a103Z0CM5IHrcyGciuq5sc0u5BpGgmavKkdrbVrMCge2JtsxuN1QmlPfQFviCJDuVqrpRS0eYx92iBZt76Oz7LdEkNnStk5RDQiotFMd/Rb/Jal" +
+        "3GIOHvMZ0ZWh2woidQtWG1tquRCCZHyPlMyazt15Z47DBy7jqglEpwSqEAGA9MbMqfkEFbvwsqzU7mH460KtOTt+zGaZXItWcRfdKOxERebdjkh/ut9KYHDk" +
+        "qIqza8HTZ/uTeU39yaxLvW0WZ7eaS7MVEX+s1ZI5nZsFsbdWmqARx2zvqAEBc2AGUvijAQ/zW2+kHbMa0y412Ws/JDMf0xUXtnpr/qrvaP13mJKKbiofupqE" +
+        "aK6ijZXMLBeIjbrXoDkUn8TgiusKXd03jTUynLJPbPAJmw2FerJuq9JzQRl/+8rmwiekajYGpCimkjB0MmiUqTvbtpyuSB+CGvBqKLlUtuGxZEO+4UjXsk+s" +
+        "3tZ4ep3RWHKRF9NmSpPbGyioGjZk3vBiHmSGzo59kZTfSdVsH/tExmYxtAJy+9Mgp2sesQs+M59DCi1ILYBdYomMXrgd2S7EVPxkn48VJ46G8hsgU3ws5cjK" +
+        "Xaw2G7Xpopb0PXLfWL9s/l3hWkIRfLaCCgvFy5hnH02weH0j4vj7+WVM+dzACmG+Bxub/6AGNLNu2UU15lR9pg1DCpsMQyr1ZDrHWpkdTLkdwTdTsSTJ7MMB" +
+        "0cc2JGu5RmLCNRuJG/UICyVd57I+UdNG92YyqEiu2UTFa1pTqNdAQXVgeLwQS6vorpqPRVH6wxC1hmXHlq99mwk7HqtBhZC6E+iBsOV7aw9NcqfsNWHbPVxQ" +
+        "buLLRR4JuBAVvmwhyvaDKvv0WmEK0ojTSE5Hb/nbTrwYZVynoskU5Ka+bU7/I09WnL0XjyJeq3vShDJqnbTGYJuxVSu9MZlK8myTrIZapbFIkkhooD3secKt" +
+        "GsB0W6idRvwBU0H3nv3uUlzLTktzhJ87VaGOaKuwMUDnGQQvWxe9FGIjzeP9ayZSXp8ig0NlSNeOjUzinCXseJ59/IgpPI8GTRSedbxjp+ZAHFtcLBWb3mV1" +
+        "WLL3cp4qzT6IzYzHeWkgcvUIrwHsCuGYyA5wR4awVKd8zysseKB5kIiZPl0i5OCgsYgWxckuZp2S0j4MCwV0psx1ydZik7Opmq84Ed4PwEPlWsj7ZEv/22LE" +
+        "XumGQ6kW0T/elbOjTMTvfL5its9CxrTiGHX4bkAuxxhYzNfSDlOLS4kvZBkWtDIY2rFXjmzfv7nBSANqI6lUjYLI1HLYik34JluLyKCSexlJ2gvY/qC9p4mc" +
+        "Q/NN7vgyRiqX3ZcV+JsUUsbWaPmE60g90K5q+z6mP8RYmy1cPlFZhZocGGDut6Ksd0pm5l9x+z1krjYzBaVQ3dYLd+6YIbGQ7CrZII2PgyZzFuiKQnNPuVnD" +
+        "SJrwlQraQMz2Z1HecPlrZiv7WEr/0miZ59ZEwKxDxikv+er9M+ct/TXlK3bOHx9ruMryTBd9TOs1j+GhGS5Vhsxagv098PE+1h6p8imG1tClVczCDn20T/HH" +
+        "s6n4TKV/G7FUuJhuLHJLhbMzofSS6CFKdGM4wITvbsW2VseekmIo3neb2JfR9QFPaWjRYJ4gOi5qIbXTTLZzNbe9L8fLWWki9bS+qzzdjWI/a4hydhXlcb4m" +
+        "PTkhEamGZNnO7ZOtrDaX4/VtNl8j3T9UUZvf7HSeaDkXdjZcwmfWnaOJuh+sF9Uono3zSJikuEr8AbmpIt+FLnpwH2JbsDKZNUX3QSVzD4wp95rP3who2r4a" +
+        "JGrhjzLltrA9p0EAKn1Jb7ecyLl5eK9VmsLBMnjp3tdVHOfsPHvkpXaXM0ra5LdAmT3RVSf8MUJGT/gNEPqBqQ+Buwidx+xapKgoy0VjYRNRVqeKMvvjH/8P" +
+        "PCrTSMAfAQA="
+}
